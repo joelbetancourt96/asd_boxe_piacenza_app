9a29ea2cce9d8078573c079f69c9a500ec167009
joelbetancourt96 <joelbetancourt96@gmial.com>
2026-04-17 12:36:20 +0200

    Commit #12 - Installazione Vue e modifica top buttons, identificazione sezione attuale

---
+++ app/javascript/application.js
@@ -0,0 +1,35 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import { createApp } from "vue"
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navbarApp = document.getElementById("navbar-app");
+  if (navbarApp) {
+    createApp({
+      data() {
+        return {
+          currentPath: window.location.pathname
+        }
+      },
+      computed: {
+        isHomeActive() {
+    
+          return this.currentPath === "/"
+        },
+        isAboutUsActive() {
+    
+          return this.currentPath === "/chi-siamo"
+        },
+        isCoursesActive() {
+    
+          return this.currentPath === "/corsi"
+        },
+        isContactsActive() {
+    
+          return this.currentPath === "/contatti"
+        }
+      }
+    }).mount(navbarApp)
+  } else {
+    console.log("Navbar app not found");
+  }
+})
