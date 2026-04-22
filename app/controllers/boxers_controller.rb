9abe9c72e21cd9ade0b63c4b10e0bac55a85735e
joelbetancourt96 <joelbetancourt96@gmial.com>
2026-04-22 16:39:37 +0200

    Commit #14 - Adattamento stile pagina pugili

--- app/controllers/boxers_controller.rb
+++ app/controllers/boxers_controller.rb
@@ -1,27 +1,30 @@
 class BoxersController < ApplicationController
   def index
-    @boxers = Profile.where(profile_type: :atleta_agonista)
-      .with_attached_profile_image
-      .includes(:athlete_detail)
-      .map do |profile|
-        athlete_detail = profile.athlete_detail
-        {
-          id: profile.id,
-          first_name: profile.first_name,
-          last_name: profile.last_name,
-          wins: athlete_detail&.wins || 0,
-          losses: athlete_detail&.losses || 0,
-          draws: athlete_detail&.draws || 0,
-          category: athlete_detail&.category || "Unknown",
-          weight: athlete_detail&.weight || 0,
-          # is_pro: athlete_detail&.is_pro || false,
-          image_url: profile_image_url(profile)
-        }
-      end
-
     respond_to do |format|
-      format.html
-      format.json { render json: @boxers }
+      format.html # Non esegue alcuna query, carica solo la pagina con Vue.js
+      format.json do
+        # Eager Loading: una sola query per profili, immagini e dettagli
+        profiles = Profile.where(profile_type: :atleta_agonista)
+                          .with_attached_profile_image
+                          .includes(:athlete_detail)
+
+        @boxers = profiles.map do |profile|
+          athlete_detail = profile.athlete_detail
+          {
+            id: profile.id,
+            first_name: profile.first_name,
+            last_name: profile.last_name,
+            wins: athlete_detail&.wins || 0,
+            losses: athlete_detail&.losses || 0,
+            draws: athlete_detail&.draws || 0,
+            category: athlete_detail&.category || "Unknown",
+            weight: athlete_detail&.weight || 0,
+            image_url: profile_image_url(profile)
+          }
+        end
+
+        render json: @boxers
+      end
     end
   end
 
